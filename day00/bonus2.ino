712ab3bd0c74c71b50223155dd7a0bb4451ed6fe
Anastasia Markina <a.markina@aerodisk.ru>
2023-11-03 20:28:08 +0100

    main and one bonus task for day00

---
+++ day00/bonus2.ino
@@ -0,0 +1,51 @@
+const int buttonPin = 2;
+const int LEDGreen = 13;
+const int LEDYellow = 12;
+const int LEDRed = 11;
+
+int buttonState = 0;
+int i = 0;
+bool pressed = false;
+
+void setup() {
+  pinMode(LEDGreen, OUTPUT);
+  pinMode(LEDYellow, OUTPUT);
+  pinMode(LEDRed, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState != pressed) {
+    digitalWrite(LEDGreen, HIGH);
+    digitalWrite(LEDRed, LOW);
+    digitalWrite(LEDYellow, LOW);
+  } else {
+      digitalWrite(LEDGreen, LOW);
+      // digitalWrite(LEDRed, HIGH);
+      digitalWrite(LEDYellow, HIGH);
+      delay(200);
+      // digitalWrite(LEDRed, LOW);
+      digitalWrite(LEDYellow, LOW);
+      while(digitalRead(buttonPin) != pressed && i < 4)
+      {
+        delay(200);
+        i++;
+      }
+      i = 0;
+      if (digitalRead(buttonPin) == pressed)
+        goto EXIT;
+      digitalWrite(LEDRed, HIGH);
+      delay(200);
+      digitalWrite(LEDRed, LOW);
+      while(digitalRead(buttonPin) != pressed && i < 4)
+      {
+        delay(200);
+        i++;
+      }
+      i = 0;
+EXIT:
+;
+  }
+}
