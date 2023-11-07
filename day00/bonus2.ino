024e48342c2fc4c7891ce44e0ef4e3ce481c3440
Anastasia Markina <a.markina@aerodisk.ru>
2023-11-07 15:32:31 +0100

    day00 altogether

--- day00/bonus2.ino
+++ day00/bonus2.ino
@@ -1,51 +1,74 @@
-const int buttonPin = 2;
-const int LEDGreen = 13;
+// constants won't change. They're used here to set pin numbers:
+const int buttonPin = 2;  // the number of the pushbutton pin
+const int LEDGreen = 13;    // the number of the LED pin
 const int LEDYellow = 12;
 const int LEDRed = 11;
+const int UP = 1;
+const int DOWN = 0;
 
-int buttonState = 0;
-int i = 0;
-bool pressed = false;
+// variables will change:
+int switching = 0;
 
 void setup() {
-  pinMode(LEDGreen, OUTPUT);
-  pinMode(LEDYellow, OUTPUT);
-  pinMode(LEDRed, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
+    // initialize the LED pin as an output:
+    pinMode(LEDGreen, OUTPUT);
+    pinMode(LEDYellow, OUTPUT);
+    pinMode(LEDRed, OUTPUT);
+    // initialize the pushbutton pin as an input:
+    pinMode(buttonPin, INPUT_PULLUP);
 }
 
-void loop() {
-  buttonState = digitalRead(buttonPin);
-
-  if (buttonState != pressed) {
+void  turn_green() {
     digitalWrite(LEDGreen, HIGH);
     digitalWrite(LEDRed, LOW);
     digitalWrite(LEDYellow, LOW);
-  } else {
-      digitalWrite(LEDGreen, LOW);
-      // digitalWrite(LEDRed, HIGH);
-      digitalWrite(LEDYellow, HIGH);
-      delay(200);
-      // digitalWrite(LEDRed, LOW);
-      digitalWrite(LEDYellow, LOW);
-      while(digitalRead(buttonPin) != pressed && i < 4)
-      {
-        delay(200);
-        i++;
-      }
-      i = 0;
-      if (digitalRead(buttonPin) == pressed)
-        goto EXIT;
-      digitalWrite(LEDRed, HIGH);
-      delay(200);
-      digitalWrite(LEDRed, LOW);
-      while(digitalRead(buttonPin) != pressed && i < 4)
-      {
-        delay(200);
-        i++;
-      }
-      i = 0;
-EXIT:
-;
-  }
+    while (digitalRead(buttonPin) == UP)
+        continue;
+    switching = 1;
+}
+
+
+void  turn_red_yellow() {
+
+    digitalWrite(LEDGreen, LOW);
+
+    while (1) {
+
+        digitalWrite(LEDYellow, HIGH);
+        digitalWrite(LEDRed, HIGH);
+        delay(50);
+        digitalWrite(LEDYellow, LOW);
+        digitalWrite(LEDRed, LOW);
+        for (int i = 0; i < 9; ++i) {
+            if (digitalRead(buttonPin) == DOWN) {
+                switching = 0;
+                return;
+            }
+            delay(50);
+        }
+        for (int j = 0; j < 3; ++j) {
+            digitalWrite(LEDRed, HIGH);
+            delay(50);
+            digitalWrite(LEDRed, LOW);
+            for (int i = 0; i < 9; ++i) {
+                if (digitalRead(buttonPin) == DOWN) {
+                    switching = 0;
+                    return;
+                }
+                delay(50);
+            }
+        }
+    }
+}
+
+void loop() {
+
+    if (switching == 0)
+        turn_green();
+    else if (switching == 1)
+        turn_red_yellow();
+
+    while(digitalRead(buttonPin) == DOWN)
+        delay(50);
+
 }
