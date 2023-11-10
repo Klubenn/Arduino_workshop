f3f9cfaa67506c4e2d6a66e87431e1166edaeadf
a.markina <a.markina@aerodisk.ru>
2023-11-10 16:42:33 +0100

    finished day01

--- day01/task.ino
+++ day01/task.ino
@@ -1,82 +1,34 @@
-/*
-  LiquidCrystal Library - Hello World
-
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
- This sketch prints "Hello World!" to the LCD
- and shows the time.
-
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- modified 7 Nov 2016
- by Arturo Guadalupi
-
- This example code is in the public domain.
-
- https://docs.arduino.cc/learn/electronics/lcd-displays
-
-*/
-
-// include the library code:
 #include <LiquidCrystal.h>
 
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 String voltage_input = "";
+#define TEXT_PIN 7
 
 void setup() {
     // set up the LCD's number of columns and rows:
     lcd.begin(16, 2);
-    // Print a message to the LCD.
-    lcd.print("hello, world!");
     Serial.begin(9600);
+    analogWrite(TEXT_PIN, 0);
 }
 
 void loop() {
-    // set the cursor to column 0, line 1
-    // (note: line 1 is the second row, since counting begins with 0):
-    lcd.setCursor(0, 1);
-    // print the number of seconds since reset:
-    // lcd.print(millis() / 1000);
 
-    // int sensorValue = analogRead(A0);
-    // // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-    // float voltage = sensorValue * (5.0 / 1023.0);
-    // // print out the value you read:
-    // // Serial.println(voltage);
-    // lcd.print(voltage);
+    int sensorValue = analogRead(A0);
+    delay(200);
 
+    // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+    float voltage = sensorValue * (5.0 / 1023.0);
+    lcd.setCursor(0,0);
+    lcd.print(voltage);
 
-    Serial.println("Enter the voltage");
-    while (Serial.available() == 0)
-        ;
-
-    voltage_input = Serial.readString();
-    lcd.clear();
-    lcd.print(voltage_input);
+    // BONUS
+    // Serial.println("Enter the voltage");
+    // while (Serial.available() == 0)
+    //   ;
+    // voltage_input = Serial.readString();
+    // lcd.clear();
+    // float voltage_converted = float(voltage_input.toInt()%256) / 255 * 5;
+    // lcd.print(voltage_converted);
+    // analogWrite(9, voltage_input.toInt()%256);
 }
