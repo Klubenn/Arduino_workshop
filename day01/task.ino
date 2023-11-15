ed2cce2b02d14ec8e5eaad06660c05ed97aa8611
a.markina <a.markina@aerodisk.ru>
2023-11-15 19:27:43 +0100

    changed day01 to work with LED

--- day01/task.ino
+++ day01/task.ino
@@ -2,33 +2,36 @@
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-String voltage_input = "";
+#define LED 9
 #define TEXT_PIN 7
 
 void setup() {
-    // set up the LCD's number of columns and rows:
     lcd.begin(16, 2);
     Serial.begin(9600);
     analogWrite(TEXT_PIN, 0);
+    pinMode(LED, OUTPUT);
 }
 
 void loop() {
-
     int sensorValue = analogRead(A0);
-    delay(200);
-
     // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
     float voltage = sensorValue * (5.0 / 1023.0);
-    lcd.setCursor(0,0);
-    lcd.print(voltage);
+
+    // analogWrite(LED, (sensorValue/4));
+    // lcd.setCursor(0,0);
+    // lcd.print(voltage);
+    // Serial.println(voltage);
+    // delay(200);
+
 
     // BONUS
-    // Serial.println("Enter the voltage");
-    // while (Serial.available() == 0)
-    //   ;
-    // voltage_input = Serial.readString();
-    // lcd.clear();
-    // float voltage_converted = float(voltage_input.toInt()%256) / 255 * 5;
-    // lcd.print(voltage_converted);
-    // analogWrite(9, voltage_input.toInt()%256);
+    Serial.println("Enter the value for brightness between 0 and 255");
+    while (Serial.available() == 0)
+        ;
+    unsigned char voltage_int = Serial.readString().toInt()%256;
+    Serial.println(voltage_int);
+    lcd.clear();
+    float voltage_converted = float(voltage_int)/ 255 * 5;
+    lcd.print(voltage_converted);
+    analogWrite(LED, voltage_int);
 }
