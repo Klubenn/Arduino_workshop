1a36d7b8eb2fecc66ce47fa421fd68e9e617d73e
pdol <79060962+gm015@users.noreply.github.com>
2023-11-17 22:15:35 +0100

    remove comments,rename vars

--- day02/task.ino
+++ day02/task.ino
@@ -1,47 +1,46 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- https://www.arduino.cc/en/Tutorial/LibraryExamples/Sweep
-*/
-
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
+Servo myservo;
 
-int pos = 0;    // variable to store the servo position
-int sensorPinM = A1; // photoresistor0
-int sensorPinL = A0; // photoresistor1
-int sensorPinR = A2; // photoresistor2
-int sensorValueM = 0;
-int sensorValueL = 0;
-int sensorValueR = 0;
+const int servo_pin = 9;
+
+int pos = 0;                              // variable to store the servo position
+int middle_sensor_pin = A1;               // photoresistor0
+int left_sensor_pin = A0;                 // photoresistor1
+int right_sensor_pin = A2;                // photoresistor2
+
+int middle_sensor_value = 0;
+int left_sensor_value = 0;
+int right_sensor_value = 0;
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(servo_pin);  // attaches the servo on pin 9 to the servo object
   Serial.begin(9600);
 }
 
 void loop() {
-  sensorValueM = analogRead(sensorPinM);
-  sensorValueL = analogRead(sensorPinL);
-  sensorValueR = analogRead(sensorPinR);
-  Serial.print(sensorValueL);
+
+  middle_sensor_value = analogRead(middle_sensor_pin);
+  left_sensor_value = analogRead(left_sensor_pin);
+  right_sensor_value = analogRead(right_sensor_pin);
+  Serial.print(left_sensor_value);
   Serial.print(" - ");
-  Serial.print(sensorValueM);
+  Serial.print(middle_sensor_value);
   Serial.print(" - ");
-  Serial.print(sensorValueR);
+  Serial.print(right_sensor_value);
   Serial.println();
 
-  pos = map(sensorValueM, 0, 1023, 0, 180);
-  myservo.write(pos);
+  pos = map(middle_sensor_value, 0, 1023, 0, 180);
+  // myservo.write(pos);
   Serial.println(pos);
   Serial.println();
   
+  // myservo.write(180);
+  // delay(1000);
+  // myservo.write(-180);
+  // delay(1000);
   delay(1000);
+
 
   // for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
   //   // in steps of 1 degree
@@ -53,6 +52,7 @@ void loop() {
   //   myservo.write(pos);              // tell servo to go to position in variable 'pos'
   //   delay(15);                       // waits 15 ms for the servo to reach the position
   // }
-  // delay(20);
+  delay(20);
 
 }
+
