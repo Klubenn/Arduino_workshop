68dca93573580db462774f933b31b5359ac64e0a
Anastasia Markina <a.markina@aerodisk.ru>
2023-11-18 17:35:27 +0100

    d02 structures

--- day02/task.ino
+++ day02/task.ino
@@ -1,6 +1,31 @@
 #include <Servo.h>
 
 Servo myservo;
+typedef struct s_sensors
+{
+    unsigned int L;
+    unsigned int M;
+    unsigned int R;
+}               t_sensors;
+
+typedef struct s_sensors
+{
+    unsigned int ;
+    unsigned char sensor_position;
+}               t_sensors;
+
+typedef struct s_sectors
+{
+    bool s0;
+    bool s1;
+    bool s2;
+    bool s3;
+    bool s4;
+    bool s5;
+    bool s6;
+    bool s7;
+    bool s8;
+}           t_sectors;
 
 const int servo_pin = 9;
 
@@ -12,47 +37,40 @@ int right_sensor_pin = A2;                // photoresistor2
 int middle_sensor_value = 0;
 int left_sensor_value = 0;
 int right_sensor_value = 0;
-
+t_sensors sensors = {0};
 void setup() {
-  myservo.attach(servo_pin);  // attaches the servo on pin 9 to the servo object
-  Serial.begin(9600);
+
+    myservo.attach(servo_pin);  // attaches the servo on pin 9 to the servo object
+    Serial.begin(9600);
 }
 
 void loop() {
+    sensors.M = analogRead(middle_sensor_pin);
+    sensors.L = analogRead(left_sensor_pin);
+    sensors.R = analogRead(right_sensor_pin);
 
-  middle_sensor_value = analogRead(middle_sensor_pin);
-  left_sensor_value = analogRead(left_sensor_pin);
-  right_sensor_value = analogRead(right_sensor_pin);
-  Serial.print(left_sensor_value);
-  Serial.print(" - ");
-  Serial.print(middle_sensor_value);
-  Serial.print(" - ");
-  Serial.print(right_sensor_value);
-  Serial.println();
+    int brightest = std::max();
+    Serial.print(sensors.L);
+    Serial.print(" - ");
+    Serial.print(sensors.M);
+    Serial.print(" - ");
+    Serial.print(sensors.R);
+    Serial.println();
 
-  pos = map(middle_sensor_value, 0, 1023, 0, 180);
-  // myservo.write(pos);
-  Serial.println(pos);
-  Serial.println();
-  
-  // myservo.write(180);
-  // delay(1000);
-  // myservo.write(-180);
-  // delay(1000);
-  delay(1000);
+    pos = map(sensors.M, 0, 1023, 0, 180);        //angle
+    // myservo.write(pos);
+    Serial.println(pos);
+    Serial.println();
+
+    // myservo.write(180);
+    // delay(1000);
+    // myservo.write(0);
+    // delay(1000);
+    // delay(1000);
 
 
-  // for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-  //   // in steps of 1 degree
-  //   myservo.write(pos);              // tell servo to go to position in variable 'pos'
-  //   delay(15);                       // waits 15 ms for the servo to reach the position
-  // }
-  // delay(20);
-  // for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-  //   myservo.write(pos);              // tell servo to go to position in variable 'pos'
-  //   delay(15);                       // waits 15 ms for the servo to reach the position
-  // }
-  delay(20);
+
+    delay(2000);
 
 }
 
